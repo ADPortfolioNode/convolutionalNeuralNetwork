2d8f0e8d2c1485685e0443ec178adf174859a83e
deoism <deofunds@gmail.com>
2024-04-09 10:23:22 -0400

    final commit

--- convNeuroNet.py
+++ convNeuroNet.py
@@ -1,31 +1,18 @@
-import tensorflow 
+"""
+This module implements a convolutional neural network.
+"""
+
+import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
 from tensorflow.keras.utils import to_categorical
-
-# import data
-from tensorflow.keras.layers import Conv2D # to add convolutional layers
-from tensorflow.keras.layers import MaxPooling2D # to add pooling layers
-from keras.layers import Flatten # to flatten data for fully connected layers
-
-# import data
-from keras.datasets import mnist
+from tensorflow.keras.datasets import mnist
 
 # load data
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 print('loading data')
-# reshape to be [samples][pixels][width][height]
-X_train = X_train.reshape(X_train.shape[0], 28, 28, 1).astype('float32')
-X_test = X_test.reshape(X_test.shape[0], 28, 28, 1).astype('float32')
-
-# import data
-from keras.datasets import mnist
-
-# load data
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
-print('data loaded')
 # reshape to be [samples][pixels][width][height]
 X_train = X_train.reshape(X_train.shape[0], 28, 28, 1).astype('float32')
 X_test = X_test.reshape(X_test.shape[0], 28, 28, 1).astype('float32')
@@ -39,7 +26,8 @@ def convolutional_model():
     
     # create model
     model = Sequential()
-    model.add(Conv2D(16, (5, 5), strides=(1, 1), activation='relu', input_shape=(28, 28, 1)))
+    model.add(Conv2D(16, (5, 5), strides=(1, 1), activation='relu',))
+    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu'))  # Reduced kernel size
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
     
     model.add(Flatten())
@@ -48,6 +36,7 @@ def convolutional_model():
     
     # compile model
     model.compile(optimizer='adam', loss='categorical_crossentropy',  metrics=['accuracy'])
+    model.save('Conv2D_model.h5')
     return model
 
 # build the model
@@ -60,38 +49,13 @@ model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_s
 scores = model.evaluate(X_test, y_test, verbose=0)
 print("Accuracy: {} \n Error: {}".format(scores[1], 100-scores[1]*100))
 
-
-def convolutional_model():
-    
-    # create model
-    model = Sequential()
-    model.add(Conv2D(16, (5, 5), activation='relu', input_shape=(28, 28, 1)))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    
-    model.add(Conv2D(8, (2, 2), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    
-    model.add(Flatten())
-    model.add(Dense(100, activation='relu'))
-    model.add(Dense(num_classes, activation='softmax'))
-    
-    # Compile model
-    model.compile(optimizer='adam', loss='categorical_crossentropy',  metrics=['accuracy'])
-    
-    return model
-
-    # build the model
-model = convolutional_model()
-
-# fit the model
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200, verbose=2)
-#model summary
+# report model summary
 model.summary()
-#model description
+# model description
 model.get_config()
-#model weights
+# model weights
 model.get_weights()
+# save model
 
-# evaluate the model
-scores = model.evaluate(X_test, y_test, verbose=0)
-print("Accuracy: {} \n Error: {}".format(scores[1], 100-scores[1]*100))
+print('model saved as conv_model.keras')
+print('>>>>>>>>>>>>>>>>End of Line<<<<<<<<<<<<<<<<<<')
